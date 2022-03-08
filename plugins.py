eb0d86975e0f82d8cc90ad8b5c2b32f10adfd7ce
Ramen-Nood1ez <55363734+Ramen-Nood1ez@users.noreply.github.com>
2022-03-08 15:53:48 -0600

    Added plugin functionality

--- plugins.py
+++ plugins.py
@@ -7,6 +7,7 @@ init(autoreset=True)
 
 curdir = os.getcwd()
 
+
 class Plugins:
     def __init__(self, mColors, file):
         self.colors = mColors
@@ -20,33 +21,36 @@ class Plugins:
 
         for i in lines:
             if len(self.store) > 0:
-                print(*self.store)
+                # print(*self.store)
                 for k in self.store:
                     self.execute(k, lines)
                 self.store = []
                 continue
             self.execute(i, lines)
 
-            
     def execute(self, line, lines):
         file = self.file
         i = line
         if i.__contains__("`"):
-            return 'comment'
+            return "comment"
         if i.__contains__("func"):
-            return 'function'
-        if i.__contains__("\t\t"):
-            return 'inside of function'
+            return "function"
+        if i.__contains__("\t") or i.__contains__("    "):
+            return "inside of function"
         if re.match("text, (\w*): ", i):
-            print(i)
+            # print(i)
             color = self.get_colors(i.split(", ")[1].split(":")[0])
             # print(i.split(", ")[1].replace(":", ""))
             print(color + i.split(": ")[1])
-            return 'text-color'
+            return "text-color"
         if i.__contains__("text: "):
             print(i)
             print(i.split("text: ")[1])
-            return 'text'
+            return "text"
+        if i.__contains__("run > "):
+            func_name = i.split("run > ")[1]
+
+            self.get_function(lines, func_name)
         if i.__contains__("prompt"):
             choices = i.split("prompt, (")[1].split(")")[0]
             destinations = i.split("> (")[1].split(")")[0].split(", ")
@@ -58,58 +62,65 @@ class Plugins:
 
             while True:
                 choice = input(">")
-                
-                print(len(lines))
+
+                # print(len(lines))
 
                 if choice.lower() in choices.lower().split(", "):
-                    for k in range(len(lines)):
-                        if lines[k] == f"func {choice.lower()} " + "{":
-                            pluginstr = ""
-                            for j in lines:
-                                pluginstr += j + "\n"
-                            
-                            print(lines[k])
-                            function = pluginstr.split("func " + choice + " {")[1].split("}")[0]
-                            print(function)
-                            
-                            for j in function.replace("    ", "").split("\n"):
-                                print(f"J: {j}")
-                                if j != "\n":
-                                    self.store.append(j)
+                    self.get_function(lines, choice.lower())
                     return
 
                 print("That is not a valid option...")
                 continue
 
             print(after_text)
-            return 'prompt'
+            return "prompt"
         if i.__contains__("wait"):
             time.sleep(int(i.split("wait, ")[1].split(":")[0]))
             print(i.split(": ")[1])
-            return 'wait'
+            return "wait"
         if i.__contains__("goto > "):
             d_chapter = i.split("goto > ", "")[1].split(":")[0] + ".txt"
             d = i.split(": ")[1] + ".txt"
             read_story(d_chapter, d)
-            return 'goto-chapter'
+            return "goto-chapter"
         if i.__contains__("goto: "):
             read_story(chapter, i.split("goto: ")[1] + ".txt")
-            return 'goto'
-        
+            return "goto"
+
+    def get_function(self, lines: list, func_name: str):
+        for k in range(len(lines)):
+            if lines[k] == f"func {func_name.lower()} " + "{":
+                pluginstr = ""
+                for j in lines:
+                    pluginstr += j + "\n"
+
+                # print(lines[k])
+                function = pluginstr.split("func " + func_name + " {")[1].split("}")[0]
+                # print(function)
+
+                for j in function.replace("    ", "").split("\n"):
+                    print(f"J: {j}")
+                    if j != "\n":
+                        self.store.append(j)
+        return
+
     def get_colors(self, color_string):
-        if color_string in colors:
+        if color_string in self.colors:
             return self.colors[color_string]
         return self.colors["white"]
-        
-        
-p = Plugins({
-    "black": Fore.BLACK,
-    "red": Fore.RED,
-    "green": Fore.GREEN,
-    "yellow": Fore.YELLOW,
-    "blue": Fore.BLUE,
-    "magenta": Fore.MAGENTA,
-    "cyan": Fore.CYAN,
-    "white": Fore.WHITE
-}, "example.plugin")
+
+
+p = Plugins(
+    {
+        "black": Fore.BLACK,
+        "red": Fore.RED,
+        "green": Fore.GREEN,
+        "yellow": Fore.YELLOW,
+        "blue": Fore.BLUE,
+        "magenta": Fore.MAGENTA,
+        "cyan": Fore.CYAN,
+        "white": Fore.WHITE,
+    },
+    "example.plugin",
+)
 p.run_plugin()
